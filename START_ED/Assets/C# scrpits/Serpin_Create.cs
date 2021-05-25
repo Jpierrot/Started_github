18e2fad0ed77542f96299a24d8cd8fdd93c24fa9
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-05-25 17:50:20 +0900

    05_25
    Serpin Complete

---
+++ START_ED/Assets/C# scrpits/Serpin_Create.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class Serpin_Create : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obj;
+    [SerializeField]
+    private GameObject obj_parent;
+    [SerializeField]
+    private float Serpin_speed;
+    [SerializeField]
+    private int Serpin_Count;
+    [SerializeField]
+    private int Serpin_Delete;
+
+    [Obsolete]
+    private void Start() {
+        CreateSerpin();
+    }
+    // Update is called once per frame
+    private void FixedUpdate() {
+
+        DeleteSerpin();
+    }
+
+    private void DeleteSerpin() {
+
+        Destroy(obj_parent.transform.GetChild(1).gameObject, Serpin_Delete);
+     }
+
+    /// <summary>
+    /// 세르핀의 생성 시간 만큼 대기
+    /// </summary>
+    /// <returns>생성 시간</returns>
+   IEnumerator waitSerpin() 
+   {
+        while (true) {
+
+            if (obj_parent.transform.childCount < Serpin_Count + 1) {
+
+                float randomX = Random.Range(-2.5f, 2.5f);
+                float randomY = Random.Range(0f, 3.5f);
+
+                GameObject my_obj = Instantiate(obj, new Vector3(randomX, randomY, 0f), Quaternion.identity);
+                my_obj.transform.parent = obj_parent.transform;
+            }
+                yield return new WaitForSeconds(Serpin_speed);
+        }
+    }
+
+    private void CreateSerpin() {
+
+        StartCoroutine(waitSerpin());
+    }
+}
