84724fa1a4ebcb7acfbf83747f12f06e6e580b17
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-05-28 11:57:22 +0900

    05_28
    Serpin_alphabet_first

--- START_ED/Assets/C# scrpits/GameManager.cs
+++ START_ED/Assets/C# scrpits/GameManager.cs
@@ -18,28 +18,69 @@ public class GameManager : MonoBehaviour
     static private int Serpin;
     // 생성량
     static private double Serpin_create = 10;
+    // 생성량 표기시
+    private double Serpin_create_TEXT;
     // 배수
     static private float Serpin_multiple = 1;
     // 레벨
     static private int Serpin_level = 1;
     // 세르핀 총량
     static private double Serpin_All;
+
+    // 세르핀 표기 총량
+    private double Serpin_ALL_TEXT;
     // 세르핀 현재 값
     static private double serpin_volume;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 세르핀 값 알파벳으로 치환
+    private char serpin_alphabet = 'A';
 
-
+    private char serpin_alphabet_for_text = 'A';
 
     // Update is called once per frame
     void Update()
     {
-        Serpin_Level_text.text = "세르핀 레벨 : " + Serpin_level.ToString() + " " + Serpin_create.ToString();
-        Serpin_text.text = "Serpin : " + Get_Serpin_ALL().ToString();
+        Serpin_Check();
+    }
+
+    public void Serpin_Check() {
+        Serpin_Alphabet_text();
+        Serpin_Alphabet_create();
+        Serpin_Level_text.text = "세르핀 레벨 : " + Serpin_level.ToString() + " " + Serpin_create_TEXT.ToString() + serpin_alphabet;
+        Serpin_text.text = "Serpin : " + Get_Serpin_ALL().ToString() + serpin_alphabet_for_text;
+    }
+
+    public void Serpin_Alphabet_create() {
+        Serpin_create_TEXT = Serpin_create;
+        int i = (int)serpin_alphabet;
+        
+
+        while (true) {
+            if (Serpin_create_TEXT < 1000) {
+                break;
+            }
+            i++;
+            serpin_alphabet = (char)i;
+            Serpin_create_TEXT -= 1000;
+        }
+        string str = string.Format(" {0 : 0}", Serpin_create_TEXT);
+        Serpin_create_TEXT = double.Parse(str);
+    }
+
+    public void Serpin_Alphabet_text() {
+        int i = (int)serpin_alphabet_for_text;
+
+
+        while (true) {
+            if (Serpin_All < 1000) {
+                break;
+            }
+            i++;
+            serpin_alphabet_for_text = (char)i;
+            Serpin_All /= 1000;
+        }
+        string str = string.Format(" {0 : 0 }", Serpin_All);
+        Serpin_All = double.Parse(str);
     }
 
     static public double Get_Serpin_ALL() {
