58591eaf19129d36ae5c36c8f886de11c3ff2e31
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-06-09 23:41:00 +0900

    06_09
    Serpin_alphabet_text
    ALL CLEAR

--- START_ED/Assets/C# scrpits/Serpin_Manager.cs
+++ START_ED/Assets/C# scrpits/Serpin_Manager.cs
@@ -25,6 +25,8 @@ public class Serpin_Manager : MonoBehaviour
     // 세르핀 값 알파벳으로 치환
     private char serpin_alphabet = 'A';
 
+    private char serpin_alphabet_ALL = 'A';
+
     // 배수
     static private float Serpin_multiple = 1;
     // 레벨
@@ -42,25 +44,18 @@ public class Serpin_Manager : MonoBehaviour
     /// </summary>
     static private double serpin_volume;
 
-    private bool serpin_checked = false;
-
-    private int serpin_alphabet_count = 0;
-
-    private char serpin_alphabet_for_text = 'A';
-
-    private void Start() {
-        Serpin_create_TEMP = Serpin_create;
-    }
     public double Serpin_create_TEXT;
-
+     
     private void Update() {
         Serpin_Check();
     }
 
     private void Serpin_Check() {
         set_Serpin_create();
+        set_Serpin_create_ALL();
         var alphabet = serpin_alphabet == '-' ? "" : serpin_alphabet.ToString();
-        Debug.Log($"{Serpin_create_TEXT} {alphabet}");
+        Serpin_Level_text.text = "세르핀 레벨  : " + $"{Serpin_level} \n생산량 : " + string.Format("{0:0.#}", Serpin_create_TEXT) + alphabet;
+        Serpin_text.text = $"세르핀 량 : " + string.Format("{0:0.#}", Serpin_ALL_TEXT) + serpin_alphabet_ALL;
     }
 
     private void set_Serpin_create() {
@@ -68,6 +63,13 @@ public class Serpin_Manager : MonoBehaviour
         Serpin_create_TEXT = Serpin_create;
 
         plus_Alphabet();
+    }
+
+    private void set_Serpin_create_ALL() {
+        serpin_alphabet_ALL = ' ';
+        Serpin_ALL_TEXT = Serpin_All;
+
+        plus_ALL_Alphabet();
     }
 
     private void plus_Alphabet() {
@@ -84,12 +86,27 @@ public class Serpin_Manager : MonoBehaviour
             plus_Alphabet();
         }
     }
+
+    private void plus_ALL_Alphabet() {
+        if (Serpin_ALL_TEXT >= 1000d) {
+            Serpin_ALL_TEXT /= 1000d;
+
+            if (serpin_alphabet_ALL == ' ')
+                serpin_alphabet_ALL = 'A';
+            else {
+                var i = (int)serpin_alphabet_ALL;
+                serpin_alphabet_ALL = (char)++i;
+            }
+
+            plus_ALL_Alphabet();
+        }
+    }
+
     static public double Get_Serpin_ALL() {
         return Serpin_All;
     }
 
     static public void Serpin_Plus() {
-
         Serpin++;
         serpin_volume = Serpin_create * Serpin_multiple;
         Serpin_All += serpin_volume;
