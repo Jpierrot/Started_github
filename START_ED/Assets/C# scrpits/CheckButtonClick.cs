5a3dabc0efd1581f34801c0e22e8ff9da8bb9bef
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-06-14 23:49:18 +0900

    06_14
    test_commit

--- START_ED/Assets/C# scrpits/CheckButtonClick.cs
+++ START_ED/Assets/C# scrpits/CheckButtonClick.cs
@@ -9,18 +9,19 @@ public class CheckButtonClick : MonoBehaviour
     private GameObject Content;
 
     [SerializeField]
-    private Text[] Trait_text = new Text[3];
+    public Text[] Trait_text = new Text[3];
 
-    private Text[] Trait_Text = new Text[9];
+    [SerializeField]
+    private Text[] Trait_Button_Text = new Text[9];
 
     [SerializeField]
     private Button[] Check_Button;
 
-    int ButtonCount=0;
+    public int ButtonCount=0;
 
     private void Start()
     {
-        Trait_Text = Content.GetComponentsInChildren<Text>();
+        Trait_Button_Text = Content.GetComponentsInChildren<Text>();
         Check_Button = Content.GetComponentsInChildren<Button>();
     }
 
@@ -32,11 +33,12 @@ public class CheckButtonClick : MonoBehaviour
             {
                 ButtonCount++;
                 Check_Button[0].transform.GetChild(0).gameObject.SetActive(true);
+                Debug.Log(Trait_Button_Text[0].text);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[0].text;break;
-                    case 2: Trait_text[1].text += Trait_Text[0].text;break;
-                    case 3: Trait_text[2].text += Trait_Text[0].text;break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[0].text;break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[0].text;break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[0].text;break;
                 }
             }
         }           
@@ -51,9 +53,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[1].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[1].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[1].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[1].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[1].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[1].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[1].text; break;
                 }
             }
         }
@@ -68,9 +70,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[2].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[2].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[2].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[2].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[2].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[2].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[2].text; break;
                 }
             }
         }
@@ -85,9 +87,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[3].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[3].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[3].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[3].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[3].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[3].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[3].text; break;
                 }
             }
         }
@@ -102,9 +104,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[4].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[4].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[4].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[4].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[4].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[4].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[4].text; break;
                 }
             }
         }
@@ -119,9 +121,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[5].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[5].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[5].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[5].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[5].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[5].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[5].text; break;
                 }
             }
         }
@@ -136,9 +138,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[6].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[6].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[6].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[6].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[6].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[6].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[6].text; break;
                 }
             }
         }
@@ -153,9 +155,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[7].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[7].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[7].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[7].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[7].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[7].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[7].text; break;
                 }
             }
         }
@@ -170,9 +172,9 @@ public class CheckButtonClick : MonoBehaviour
                 Check_Button[8].transform.GetChild(0).gameObject.SetActive(true);
                 switch (ButtonCount)
                 {
-                    case 1: Trait_text[0].text += Trait_Text[8].text; break;
-                    case 2: Trait_text[1].text += Trait_Text[8].text; break;
-                    case 3: Trait_text[2].text += Trait_Text[8].text; break;
+                    case 1: Trait_text[0].text += Trait_Button_Text[8].text; break;
+                    case 2: Trait_text[1].text += Trait_Button_Text[8].text; break;
+                    case 3: Trait_text[2].text += Trait_Button_Text[8].text; break;
                 }
             }
         }
--- START_ED/Assets/C# scrpits/EmployButtonClick.cs
+++ START_ED/Assets/C# scrpits/EmployButtonClick.cs
@@ -10,13 +10,26 @@ public class EmployButtonClick : MonoBehaviour
     [SerializeField]
     private GameObject Employ_TOP_layout;
     [SerializeField]
+    private GameObject InterView_TOP_layout;
+    [SerializeField]
     private GameObject MIDDLE_layout;
     [SerializeField]
     private GameObject Trait_MIDDLE_layout;
     [SerializeField]
+    private GameObject InterView_MIDDLE_layout;
+    [SerializeField]
     private GameObject serpins;
     [SerializeField]
     private Text Serpin_text;
+    [SerializeField]
+    private GameObject Trait_text_boxes;
+    [SerializeField]
+    private Text[] Trait_text;
+
+    private void Start()
+    {
+        Trait_text = Trait_text_boxes.GetComponentsInChildren<Text>();
+    }
 
     public void EmployButtonOnClick()
     {
@@ -28,12 +41,18 @@ public class EmployButtonClick : MonoBehaviour
         Serpin_text.gameObject.SetActive(false);
     }
 
-    public void UnEmployButtonOnClick() {
-        TOP_layout.SetActive(true);
-        Employ_TOP_layout.SetActive(false);
-        MIDDLE_layout.SetActive(true);
-        Trait_MIDDLE_layout.SetActive(false);
-        serpins.transform.gameObject.SetActive(true);
-        Serpin_text.gameObject.SetActive(true);
+    public void EmployButton1OnClick() {
+        CheckButtonClick buttonClick = GameObject.Find("Canvas").GetComponent<CheckButtonClick>();
+        if (buttonClick.ButtonCount >= 3)
+        {
+            Employ_TOP_layout.SetActive(false);
+            InterView_TOP_layout.SetActive(true);
+            Trait_MIDDLE_layout.SetActive(false);
+            InterView_MIDDLE_layout.SetActive(true);
+
+            Trait_text[0].text = buttonClick.Trait_text[0].text;
+            Trait_text[1].text = buttonClick.Trait_text[1].text;
+            Trait_text[2].text = buttonClick.Trait_text[2].text;
+        }
     }
 }
