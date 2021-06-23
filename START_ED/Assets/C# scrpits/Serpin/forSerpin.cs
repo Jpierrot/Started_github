0de907c2c515fd11412da8b014c12aff2ca23cff
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-06-23 17:17:53 +0900

    06/23
    test

---
+++ START_ED/Assets/C# scrpits/Serpin/forSerpin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forSerpin : MonoBehaviour
+{
+    private void OnMouseDown() {
+
+        Serpin_Manager.Serpin_Plus();
+        Destroy(this.gameObject);
+    }
+}
